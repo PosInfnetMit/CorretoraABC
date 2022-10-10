5dccbea3488ed329cb45fe51b151281f4a08131e
Elder da Silva Souza <elderonline@hotmail.com>
2022-10-10 14:49:45 -0300

    Criação do contexto para gerar base; Criação do projeto web para apresentar os graficos.

---
+++ Corretora.Repository/Context/CorretoraContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corretora.Repository.Context
+{
+    public class CorretoraContext : DbContext
+    {
+        public CorretoraContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {            
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CorretoraContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+
+    }
+}
